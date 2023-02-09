0c4df515d137595c93979a56eed909192ec42a48
Ahnm1 <hansson.alexander@live.se>
2023-02-09 19:30:06 +0100

    Script for creating folder structure

---
+++ make_tree.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir data_w
+mkdir data_el
+
+mkdir data_w/raw 
+mkdir data_el/raw
+
+mkdir data_w/target
+mkdir data_el/harmonized
+
+mkdir data_w/merged
+mkdir data_el/merged 
+
+mkdir data_w/clean 
+mkdir data_el/clean 
+
+mkdir data_w/final_avg
