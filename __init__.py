67eec8d668e50a6f01da2e2880164da1b9ef8902
Ahnm1 <hansson.alexander@live.se>
2023-02-10 10:15:12 +0100

    Working as should

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
+import os
 from EnergyHandler import EnergyHandler
 from MainSMHI import MainSMHI
+from MegaMerger import MegaMerger
 
 start_date = '2021-11-01 00:00'
 end_date   = '2022-11-01 00:00'
@@ -13,11 +15,17 @@ e_handle.clean_price_data('raw_el_prices.json')
 
 # Weather
 smhi = MainSMHI()
-
 smhi.get_raw()
 smhi.get_raw('wind')
 smhi.clean_raw()
 smhi.save_samples('2021-11-01', '2022-10-31')
-# smhi.merge_samples() # Not yet done
+
 
 # Merger
+targets = os.listdir('data_w/target/')
+
+mm = MegaMerger()
+mm.calculate_averages(targets)
+mm.merge_weather_and_prices(
+    'Vindhastighet_AVG.csv', 'Lufttemperatur_AVG.csv', 'harmonized_el_prices.csv')
+mm.find_and_fill_missing_data()
