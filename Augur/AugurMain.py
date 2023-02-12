db0efb4b72837674d565726b06a4c213493e5830
Ahnm1 <hansson.alexander@live.se>
2023-02-12 16:26:17 +0100

    Centers window on screen, welcome text added.

--- Augur/AugurMain.py
+++ Augur/AugurMain.py
@@ -17,8 +17,22 @@ predict_var = tk.StringVar()
 # date_var.set(7)
 
 ## This is a hack to keep UI consistent from start to finish
-predict_var.set('\n\n\n\n\n\n\n\n')          # Don't touch it!
+predict_var.set('Use average values\nas input for\nwind and temeprature.\n\n\n\n\n\n')          # Don't touch it!
 
 AugurUI(root).set_ui(wind_var, temp_var, date_var, hour_var, predict_var)
 
+## Center window
+# Gets the requested values of the height and widht.
+windowWidth = root.winfo_reqwidth()
+windowHeight = root.winfo_reqheight()
+print("Width",windowWidth,"Height",windowHeight)
+ 
+# Gets both half the screen width/height and window width/height
+positionRight = int(root.winfo_screenwidth()/2 - windowWidth/2)
+positionDown  = int(root.winfo_screenheight()/2 - windowHeight/2) - 160
+ 
+# Positions the window in the center of the page.
+root.geometry("+{}+{}".format(positionRight, positionDown))
+
+# Start program
 root.mainloop()
