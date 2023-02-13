9dc243f08433feab0e46e879817e3a2742840a69
Ahnm1 <hansson.alexander@live.se>
2023-02-13 14:40:27 +0100

    Implemented insertion to db.

--- __init__.py
+++ __init__.py
@@ -1,16 +1,21 @@
 import os
 from tkinter import messagebox
+import pandas as pd
+from pkgs import mkdirs_win
 from pkgs.EnergyHandler import EnergyHandler
 from pkgs.MainSMHI import MainSMHI
 from pkgs.MegaMerger import MegaMerger
+from Database_queries.DBWorker import DBWorker
 
 start_date = '2021-11-01 00:00'
 end_date   = '2022-11-01 00:00'
-SE_AREA    = 'SE4'
 params     = ['1', '4']
 
+SE_AREA    = 'SE4'
+CURR_DIR_PATH = os.path.dirname(os.path.realpath('__file__'))
+
 start_now = messagebox.askquestion(
-    'askquestion', 
+    'Are you sure?', 
     'Will fetch data from remote servers.\n'+ \
         '~100MB\nProceed anyway?')
 
@@ -19,9 +24,9 @@ if start_now == 'no':
     exit()
 
 else:
-    
+
     ## Make directory tree if not present
-    os.system('./pkgs/make_tree.sh')
+    mkdirs_win.make_directory_tree(CURR_DIR_PATH)
 
     ## Prices 
     e_handle = EnergyHandler()
@@ -44,5 +49,19 @@ else:
         'Vindhastighet_AVG.csv', 'Lufttemperatur_AVG.csv', 'harmonized_el_prices.csv')
     mm.find_and_fill_missing_data()
 
-messagebox.showinfo('showinfo', 'Done!\nFile saved in:\n' + \
+    # Preload data before database
+    fact_df = pd.read_csv(CURR_DIR_PATH + "\\data_el\\merged\\nonull_elpriser_och_vader.csv", sep=';')
+    fact_data = ""
+
+    for index, row in fact_df.iterrows():
+        row_text = "('" + row[0] + "'," + str(row[4]) + "," + str(row[3]) + ",'" + row[1] + "'," + str(row[2]) + "),"
+        fact_data = fact_data + "\n" + row_text
+    fact_data = fact_data[:-1]
+   
+    ## Connect and do db work
+    db_worker = DBWorker()
+    conn = db_worker.connect_to_db()
+    db_worker.write_to_fact(conn, fact_data)
+
+messagebox.showinfo('Done!', 'File saved in:\n' + \
     'data_el/merged/\nas:\nnonull_elpriser_och_vader.csv')
