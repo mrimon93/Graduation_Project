effdc6b82e0083891f69575eb722e7469349ebc5
Ahnm1 <hansson.alexander@live.se>
2023-02-13 17:00:20 +0100

    Improved test

--- Augur/ML/MLPredict.py
+++ Augur/ML/MLPredict.py
@@ -2,9 +2,9 @@
 #  Load a trained model from a file and use it to make predictions. The script should take the following input from the user:
 #  Vindhastighet AVG, Lufttemperatur AVG, Month, TimeOfDay. The script should then use the model to make a prediction of the price of electricity in the future.
 #  The script should then print the prediction to the user.
-
+import time
 import pandas as pd
-# import numpy as np
+import numpy as np
 import pickle
 from sklearn.preprocessing import StandardScaler
 import currency_converter as cc
@@ -45,6 +45,23 @@ def get_input_and_predict(wind:float, temp:float, month:int, hour:int):
     return float(y_pred.round(1)[0]) # y_pred
 
 if __name__ == '__main__':
-    get_input_and_predict(wind = 2, temp = 12, month = 4, hour = 4)
-    get_input_and_predict(wind = 2, temp = 12, month = 6, hour = 4)
-    get_input_and_predict(wind = 2, temp = 12, month = 8, hour = 4)
+    runnum = 0
+    st = time.time()
+
+    for i in range(0,11):
+        runnum += 1
+        wind  = np.random.randint(0, 14)
+        temp  = np.random.randint(-10, 24)
+        month = np.random.randint(1, 12)
+        hour  = np.random.randint(0, 23)
+
+        print('\nTest:', runnum)
+        print(f'Hour: {hour}, Temp: {temp}, Wind: {wind}, Month: {month}')
+
+        get_input_and_predict(wind = wind, temp = temp, month = month, hour = hour)
+
+    end_t = time.time()
+    print('Executed in:', (end_t-st), 'seconds')
+
+    # get_input_and_predict(wind = 4, temp = 12, month = 6, hour = 4)
+    # get_input_and_predict(wind = 6, temp = 12, month = 8, hour = 4)
